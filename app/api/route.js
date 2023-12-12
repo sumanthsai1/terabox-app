20dc9b08a43098b46d133585f564506782744d0e
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2023-12-12 17:33:18 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -34,7 +34,7 @@ const headers = {
   Cookie:
     "csrfToken=x0h2WkCSJZZ_ncegDtpABKzt; browserid=Bx3OwxDFKx7eOi8np2AQo2HhlYs5Ww9S8GDf6Bg0q8MTw7cl_3hv7LEcgzk=; lang=en; TSID=pdZVCjBvomsN0LnvT407VJiaJZlfHlVy; __bid_n=187fc5b9ec480cfe574207; ndus=Y-ZNVKxteHuixZLS-xPAQRmqh5zukWbTHVjen34w; __stripe_mid=895ddb1a-fe7d-43fa-a124-406268fe0d0c36e2ae; ndut_fmt=FF870BBFA15F9038B3A39F5DDDF1188864768A8E63DC6AEC54785FCD371BB182",
   DNT: "1",
-  Host: "www.4funbox.com",
+  Host: "www.1024tera.com",
   "Sec-Fetch-Dest": "document",
   "Sec-Fetch-Mode": "navigate",
   "Sec-Fetch-Site": "none",
@@ -48,6 +48,22 @@ const headers = {
   "sec-ch-ua-platform": '"Windows"',
 };
 
+// ... (existing functions and headers remain the same)
+
+async function sendMessage(chatId, text) {
+  const botToken = "6967803453:AAESYXs9tO8nUazRVLw8dL8h-RozIHjGx80"; // Replace with your bot token
+  const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  const params = new URLSearchParams({
+    chat_id: chatId,
+    text: text,
+  });
+
+  const response = await fetch(`${apiUrl}?${params.toString()}`);
+  const data = await response.json();
+  return data;
+}
+
 export async function GET(req, res) {
   const { searchParams: params } = new URL(req.url);
   if (!params.has("data")) {
@@ -59,9 +75,6 @@ export async function GET(req, res) {
   }
   const secretKey = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d";
   let url;
-  let decryptedBytes;
-  const turl = `https://t.me/all_links_downloadbot`; 
-  
   try {
     const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
     const decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
@@ -73,6 +86,10 @@ export async function GET(req, res) {
     }
   } catch (error) {
     console.error("Decryption error:", error);
+
+    const chatId = "6967803453"; // Replace with the actual chatId
+    await sendMessage(chatId, `Error: Invalid encrypted data`);
+
     return NextResponse.json(
       { error: "Invalid encrypted data" },
       { status: 400 }
@@ -84,12 +101,18 @@ export async function GET(req, res) {
     const jsToken = findBetween(responseData, "fn%28%22", "%22%29");
     const logid = findBetween(responseData, "dp-logid=", "&");
     if (!jsToken || !logid) {
+      const chatId = "6967803453"; // Replace with the actual chatId
+      await sendMessage(chatId, `Error: Invalid response`);
+
       return NextResponse.json({ error: "Invalid response" }, { status: 400 });
     }
     const { searchParams: requestUrl, href } = new URL(
       req.request.res.responseUrl
     );
     if (!requestUrl.has("surl")) {
+      const chatId = "6967803453"; // Replace with the actual chatId
+      await sendMessage(chatId, `Error: Missing data`);
+
       return NextResponse.json({ error: "Missing data" }, { status: 400 });
     }
     const surl = requestUrl.get("surl");
@@ -110,17 +133,34 @@ export async function GET(req, res) {
       root: "1",
     };
 
-    const req2 = await axios.get("https://www.4funbox.com/share/list", {
+    const req2 = await axios.get("https://www.1024tera.com/share/list", {
       params,
       headers,
       withCredentials: true,
     });
     const responseData2 = req2.data;
-    if (!"list" in responseData2) {
+    if (!("list" in responseData2)) {
+      const chatId = "6967803453"; // Replace with the actual chatId
+      await sendMessage(chatId, `Error: Invalid response`);
+
       return NextResponse.json({ error: "Invalid response" }, { status: 400 });
     }
-    return NextResponse.json({ message: responseData2?.list[0] }, { status: 200 });
+
+    // Assuming you have a chatId from the Telegram message
+    const chatId = "6967803453"; // Replace with the actual chatId
+
+    // Sending the final URL to the Telegram bot
+    const finalUrl = responseData2?.list[0]?.dlink || "Error: Final URL not available";
+    await sendMessage(chatId, `Final URL: ${finalUrl}`);
+
+    return NextResponse.json(responseData2?.list[0], { status: 200 });
   } catch (error) {
+    console.error("Error:", error);
+
+    // Sending an error message to the Telegram bot
+    const chatId = "6967803453"; // Replace with the actual chatId
+    await sendMessage(chatId, `Error: ${error.message}`);
+
     return NextResponse.json({ error: "Unknown Error" }, { status: 400 });
   }
 }
