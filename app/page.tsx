8636b2ad3f19ecf0053889fb4590236972a8fa72
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2023-12-12 09:15:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 "use client";
-import { useState, useEffect } from "react";
-import useSWR from "swr";
-import CryptoJS from "crypto-js";
-import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+import CryptoJS from "crypto-js";
+import Image from "next/image";
+
+const fetchWithToken = async (url: URL | RequestInfo) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const errorRes = await res.json();
+    const error = new Error();
+    error.message = errorRes?.error;
+    throw error;
+  }
+
+  return await res.json();
+};
 
 function getFormattedSize(sizeBytes: number) {
   let size, unit;
@@ -23,17 +35,6 @@ function getFormattedSize(sizeBytes: number) {
 
   return `${size.toFixed(2)} ${unit}`;
 }
-const fetchWithToken = async (url: URL | RequestInfo) => {
-  const res = await fetch(url);
-  if (!res.ok) {
-    const errorRes = await res.json();
-    const error = new Error();
-    error.message = errorRes?.error;
-    throw error;
-  }
-
-  return await res.json();
-};
 
 function convertEpochToDateTime(epochTimestamp: number) {
   const normalDate = new Date(epochTimestamp * 1000);
@@ -97,14 +98,12 @@ function checkUrlPatterns(url: string) {
 
   return false;
 }
-const SECRET_KEY = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d";
-
 
 export default function Home() {
-  const [links, setLinks] = useState("");
+  const [link, setLink] = useState("");
   const [err, setError] = useState("");
   const [token, setToken] = useState("");
-  const [disableInput, setDisableInput] = useState(false);
+  const [disableInput, setdisableInput] = useState(false);
 
   const { data, error, isLoading } = useSWR(
     token ? [`/api?data=${encodeURIComponent(token)}`] : null,
@@ -115,95 +114,114 @@ export default function Home() {
       revalidateOnReconnect: false,
     }
   );
-
+  
   useEffect(() => {
-    if (error) {
-      setDisableInput(false);
-      setLinks("");
+    if (data || error) {
+      setdisableInput(false);
+      setLink("");
+    }
+    if (err || error) {
       setTimeout(() => {
         setError("");
       }, 5000);
     }
-  }, [error]);
+  }, [err, error, data]);
 
-  async function submit() {
+  async function Submit() {
     setError("");
-    setDisableInput(true);
-
-    const linksArray = links.split(",").map((link) => link.trim());
-
-    if (!linksArray.length) {
-      setError("Please enter at least one link");
-      setDisableInput(false);
+    setdisableInput(true);
+    if (!link) {
+      setError("Please enter a link");
       return;
     }
-
-    try {
-      const expirationTime = Date.now() + 20000;
-      const dataToEncrypt = JSON.stringify({
-        token: linksArray,
-        expiresAt: expirationTime,
-      });
-
-      const encryptedData = CryptoJS.AES.encrypt(
-        dataToEncrypt,
-        SECRET_KEY
-      ).toString();
-
-      setToken(encryptedData);
-    } catch (error) {
-      console.error("Encryption error:", error);
-      setError("Error during encryption");
-      setDisableInput(false);
+    if (!checkUrlPatterns(link)) {
+      setError("Invalid Link");
+      return;
     }
+    const secretKey = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d";
+    const expirationTime = Date.now() + 20000;
+    const dataToEncrypt = JSON.stringify({
+      token: link,
+      expiresAt: expirationTime,
+    });
+    const encryptedData = CryptoJS.AES.encrypt(
+      dataToEncrypt,
+      secretKey
+    ).toString();
+    setToken(encryptedData);
   }
 
   return (
-  <div className="pt-6 mx-12">
-    <header className="mb-6">
-      <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">
-        Downloader
-      </h1>
-    </header>
-    <main className="flex flex-col items-center justify-center space-y-8">
-      <div className="self-center text-black">
-        <Input
-          disabled={disableInput}
-          className="max-w-80"
-          placeholder="Enter the links (separated by commas)"
-          onChange={(e) => setLinks(e.target.value)}
-        />
-      </div>
-      <div className="self-center">
-        <Button
-          className="bg-green-600"
-          disabled={disableInput}
-          onClick={submit}
-        >
-          {isLoading && (
-            <div role="status">
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* ... (loading SVG remains the same) */}
-              </svg>
-              <span className="sr-only">Loading...</span>
-            </div>
-          )}
-          Download
-        </Button>
-      </div>
-      {error && (
-        <p className="bg-rose-500 text-white w-full text-center">
-          {error.message}
-        </p>
-      )}
+    <div className="pt-6 mx-12">
+      <nav className="flex justify-between ">
+        <div className="self-center">
+          <Link href="/">Terabox Downloader</Link>
+        </div>
+        <ul>
+          <li>
+            {/* <Camera color="red" size={48} /> */}
+            <Button className="bg-blue-600">
+              <Link href="https://t.me/RoldexVerse">Telegram</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+      <main className="mt-6 py-10 bg-slate-700 rounded-lg items-center flex flex-col justify-center gap-2">
+        <h1 className="text-xl sm:text-3xl font-bold text-center text-white">
+          Terabox Downloader
+        </h1>
+        <p className="text-center text-white">Enter your Terabox link below</p>
+        <div className="flex flex-col justify-center ">
+          <div className="self-center text-black">
+            <Input
+              disabled={disableInput}
+              className="max-w-80"
+              placeholder="Enter the link"
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="self-center">
+          <Button
+            className="bg-green-600"
+            disabled={disableInput}
+            onClick={Submit}
+          >
+            {isLoading && (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
+            Download
+          </Button>
+        </div>
+        {error && (
+          <p className="bg-rose-500 text-white w-full text-center">
+            {error.message}
+          </p>
+        )}
+        {err && (
+          <p className="bg-rose-500 text-white w-full text-center">{err}</p>
+        )}
+      </main>
       {data && (
-        <div className="my-10 py-10 bg-slate-700 rounded-lg items-start flex flex-col justify-start gap-2">
+        <main className="my-10 py-10 bg-slate-700 rounded-lg items-start flex flex-col justify-start gap-2">
           <div className="w-full">
             <div className="rounded-md flex justify-center items-center ">
               <Image
@@ -213,7 +231,7 @@ export default function Home() {
                 src={data?.thumbs?.url1}
                 height={200}
                 width={200}
-                alt=""
+                alt={""}
               />
             </div>
           </div>
@@ -227,7 +245,7 @@ export default function Home() {
             </h1>
             <h1 className="text-sm lg:text-xl text-white ">
               File Size:{" "}
-              <span className="text-white  text-md lg:text-2xl font-bold ">
+              <span className="text-white text-md lg:text-2xl font-bold ">
                 {getFormattedSize(data.size)}
               </span>
             </h1>
@@ -252,9 +270,8 @@ export default function Home() {
               Download
             </Button>
           </Link>
-        </div>
+        </main>
       )}
-    </main>
-  </div>
-);
+    </div>
+  );
 }
