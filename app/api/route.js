cc0b81001a6e800a37bdca7f081bd6e9e82296f5
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2023-12-12 16:42:55 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
+
 import CryptoJS from "crypto-js";
 
 function getFormattedSize(sizeBytes) {
@@ -52,21 +53,18 @@ export async function GET(req, res) {
   if (!params.has("data")) {
     return NextResponse.json({ error: "Missing data" }, { status: 400 });
   }
-
   const encryptedData = params.get("data");
   if (!encryptedData) {
     return NextResponse.json({ error: "Missing data" }, { status: 400 });
   }
-
   const secretKey = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d";
-  let urls;
-
+  let url;
   try {
     const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
     const decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
     const { token: decryptedToken, expiresAt } = JSON.parse(decryptedData);
-    urls = decryptedToken.split(',').map(url => url.trim()); // Split by comma
-
+    url = decryptedToken;
+    console.log(url, expiresAt);
     if (Date.now() > expiresAt) {
       return NextResponse.json({ error: "Expired token" }, { status: 401 });
     }
@@ -77,67 +75,49 @@ export async function GET(req, res) {
       { status: 400 }
     );
   }
+  try {
+    const req = await axios.get(url, { headers, withCredentials: true });
+    const responseData = req.data;
+    const jsToken = findBetween(responseData, "fn%28%22", "%22%29");
+    const logid = findBetween(responseData, "dp-logid=", "&");
+    if (!jsToken || !logid) {
+      return NextResponse.json({ error: "Invalid response" }, { status: 400 });
+    }
+    const { searchParams: requestUrl, href } = new URL(
+      req.request.res.responseUrl
+    );
+    if (!requestUrl.has("surl")) {
+      return NextResponse.json({ error: "Missing data" }, { status: 400 });
+    }
+    const surl = requestUrl.get("surl");
 
-  // Iterate through URLs and download
-  const downloadResults = await Promise.allSettled(
-    urls.map(async (url) => {
-      try {
-        const req = await axios.get(url, { headers, withCredentials: true });
-        const responseData = req.data;
-        const jsToken = findBetween(responseData, "fn%28%22", "%22%29");
-        const logid = findBetween(responseData, "dp-logid=", "&");
-        if (!jsToken || !logid) {
-          return { error: `Invalid response from ${url}` };
-        }
-        const { searchParams: requestUrl, href } = new URL(req.request.res.responseUrl);
-        if (!requestUrl.has("surl")) {
-          return NextResponse.json({ error: "Missing data" }, { status: 400 });
-        }
-        const surl = requestUrl.get("surl");
+    const params = {
+      app_id: "250528",
+      web: "1",
+      channel: "dubox",
+      clienttype: "0",
+      jsToken: jsToken,
+      dplogid: logid,
+      page: "1",
+      num: "20",
+      order: "time",
+      desc: "1",
+      site_referer: href,
+      shorturl: surl,
+      root: "1",
+    };
 
-        // Handle the case where 'auth' is not present in the response
-        const { auth } = responseData || {};
-
-        const params = {
-          app_id: "250528",
-          web: "1",
-          channel: "dubox",
-          clienttype: "0",
-          jsToken: jsToken,
-          dplogid: logid,
-          page: "1",
-          num: "20",
-          order: "time",
-          desc: "1",
-          site_referer: href,
-          shorturl: surl,
-          root: "1",
-        };
-
-        // Use 'auth' in the request if available, or provide an empty object as a fallback
-        const req2 = await axios.get("https://www.1024tera.com/share/list", {
-          params,
-          headers,
-          withCredentials: true,
-          auth: auth || {}, // Provide default empty object if 'auth' is not present
-        });
-
-        // ... (rest of the code to download without displaying details)
-        // Replace or modify as needed for your specific use case
-      } catch (error) {
-        return { error: `Error downloading ${url}` };
-      }
-    })
-  );
-
-  const errors = downloadResults
-    .filter((result) => result.status === 'rejected')
-    .map((result) => result.reason.error);
-
-  if (errors.length > 0) {
-    return NextResponse.json({ errors }, { status: 400 });
+    const req2 = await axios.get("https://www.1024tera.com/share/list", {
+      params,
+      headers,
+      withCredentials: true,
+    });
+    const responseData2 = req2.data;
+    if (!"list" in responseData2) {
+      return NextResponse.json({ error: "Invalid response" }, { status: 400 });
+    }
+    return NextResponse.json(responseData2?.list[0], { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Unknown Error" }, { status: 400 });
   }
-
-  return NextResponse.json({ success: true }, { status: 200 });
-
 }
