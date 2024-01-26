82c8396ec22db17bfa1778aed50c5915d94d7616
sumanthsai1 <41504632+sumanthsai1@users.noreply.github.com>
2024-01-26 16:11:34 +0530

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -116,6 +116,7 @@ export async function GET(req, res) {
     if (!"list" in responseData2) {
       return NextResponse.json({ error: "Invalid response" }, { status: 400 });
     }
+    console.log(responseData2);
     return NextResponse.json(responseData2?.list[0], { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Unknown Error" }, { status: 400 });
